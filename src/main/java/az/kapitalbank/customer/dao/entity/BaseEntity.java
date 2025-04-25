1f8158d53fb5cf054ac5b7703933eb29ceff637e
aghayevali <ali.aghayev@guavapay.com>
2025-04-25 10:30:44 +0400

    Add Customer entity, repository, and Liquibase migration
    - Created CustomerEntity extending BaseEntity with fields: name, surname, phone number, birth date, and balance.
    - Added CustomerRepository to handle CRUD operations for CustomerEntity.
    - Implemented Liquibase migration to create the 'customers' table with initial structure.

---
+++ src/main/java/az/kapitalbank/customer/dao/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package az.kapitalbank.customer.dao.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/az/kapitalbank/customer/dao/entity/CustomerEntity.java
@@ -0,0 +1,32 @@
+package az.kapitalbank.customer.dao.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+public class CustomerEntity extends BaseEntity {
+    private String name;
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    private BigDecimal balance = BigDecimal.valueOf(100);
+}
